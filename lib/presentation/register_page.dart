335e0a09fc5212d6de1994777066c7807145e8ea
abrarimamsatria09 <092004abrar@gmail.com>
2025-04-22 20:09:36 +0700

    membuat text register dan textformfield email

--- lib/presentation/register_page.dart
+++ lib/presentation/register_page.dart
@@ -15,9 +15,30 @@ class _RegisterPageState extends State<RegisterPage> {
   void initState() {
     super.initState();
   }
-  
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Form(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              Text('Register'),
+              TextFormField(
+                controller: emailController,
+                decoration: const InputDecoration(labelText: 'Email'),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your email';
+                  }
+                  return null;
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
